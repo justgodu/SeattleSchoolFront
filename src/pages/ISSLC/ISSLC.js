f9b8c5852442f6a060e3d6ef5a97757c128bad8a
Nika Kiria <nika.kiria@shopshop.ge>
2022-12-07 13:04:02 +0400

    add schools sort logic by name and type

--- src/pages/ISSLC/ISSLC.js
+++ src/pages/ISSLC/ISSLC.js
@@ -11,7 +11,7 @@ function ISSLC(props){
 
         const getAndSetSchools = async () => {
             let schoolList = await getSchools({
-                select: "_id|name"
+                select: "_id|name|school_type"
             });
             setSchools(schoolList.items);
         }
@@ -19,6 +19,43 @@ function ISSLC(props){
         getAndSetSchools();
 
     }, [])
+
+    const SortButtons = [
+        {
+            "title": "School Name",
+            "bgcolor": "#006AA3",
+            "txtcolor": "#FFFFFF",
+            "key": "school_name"
+        },
+        {
+            "title": "School Type",
+            "bgcolor": "#006AA3",
+            "txtcolor": "#FFFFFF",
+            "key": "school_type"
+        },
+    ];
+
+    // Sort schools
+    const onClickSort = async (sortKey)=>{
+
+        let sortedList;
+
+        // Sort schools alphabetically by name
+        if (sortKey === "school_name") {
+            sortedList = schools.sort((a,b) => a.name.localeCompare(b.name));
+        }
+        // Sort schools alphabetically by type
+        else if (sortKey === "school_type") {
+            sortedList = schools.sort((a,b) => a.school_type.localeCompare(b.school_type));
+        } else {
+            return;
+        }
+
+        setSchools([...sortedList]);
+        
+    }
+
+
     return(
         <>
             <Header title={"ISSLC Standards Dashboard"} />
@@ -26,6 +63,20 @@ function ISSLC(props){
                 <div className={"edit-form-info container"}>
                     <h3><Link to={"/"}>Dashboard</Link></h3>
                     <h3>ISSLC</h3>
+                </div>
+                <div className={"filters-container container schools-filter-container"}>
+                    <h4>Sort by</h4>
+                    {
+                        SortButtons.map((item, index)=>(
+                            <button key={index} className={"sort-item school-sort-item"} onClick={()=>onClickSort(item.key)} >
+                                <div className={"sort-item-container"} style={{backgroundColor: item.bgcolor}}>
+                                    <h4 style={{color: item.txtcolor}}>
+                                        {item.title}
+                                    </h4>
+                                </div>
+                            </button>
+                        ))
+                    }
                 </div>
                 <div className={"selcet-school"}>
                     <h3>
@@ -36,16 +87,20 @@ function ISSLC(props){
                     <table border={0} cellSpacing={0} className={"school-table"}>
                         <tbody>
                         <tr>
-                            <td className={"table-header"}>Schools</td>
+                            <td className={"table-header school-table-header"}>Schools</td>
+                            <td className={"table-header school-table-header"}>School Type</td>
                         </tr>
                         {
                             schools.length && schools.map((school, index)=> (
                                     <tr key={index}>
-                                        <th className={"row-header-" + index%2 + " schools-table-row"} scope={"row"}>
+                                        <td className={"row-header-" + index%2 + " schools-table-row"} scope={"row"}>
                                             <Link to={"/isslc/" + school._id} className={"school-link"}>
                                                 {school.name}
                                             </Link>
-                                        </th>
+                                        </td>
+                                        <td className={"row-header-" + index%2 + " schools-table-row"} scope={"row"}>
+                                            {school.school_type}
+                                        </td>
                                     </tr>
                                 )
                             )
